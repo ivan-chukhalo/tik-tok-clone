51756debd25a0cde05283128de7c4ad2b5556b93
Ivan_Chukhalo <ivanchukhalo@gmail.com>
2023-02-26 00:36:29 +0200

    feat: acomplish developing cards with video but with interface elements

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Video.css";
 
 export default function Video() {
-  return <div className="video">
-    <h1 className="video__header">This is a video</h1>
-    {/* <VideoFooter /> */}
-    {/* <VideoSidebar /> */}
-  </div>;
+    const [playing, setPlaying] = React.useState(false)
+    const videoRef = useRef(null)
+    const onVideoPress = () => {
+      if (playing){
+        videoRef.current.pause();
+        setPlaying(false)
+      } else {
+        videoRef.current.play();
+        setPlaying(true)
+      }
+    }
+    return (
+    <div className="video">
+      <video
+        src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/av1/360/Big_Buck_Bunny_360_10s_20MB.mp4"
+        className="video__player"
+        loop
+        autoPlay
+        ref={videoRef}
+        onClick={onVideoPress}
+      ></video>
+
+      {/* <VideoFooter /> */}
+      {/* <VideoSidebar /> */}
+    </div>
+  );
 }
