2a2df42353a65cc60e01250d58118e4bc83fbd03
Ivan_Chukhalo <ivanchukhalo@gmail.com>
2023-02-25 13:03:45 +0200

    init: initial commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBINaZmtMd_lU1vMXQJxzo4Nn8ILQDMmAM",
+  authDomain: "tik-tok-clone-12d14.firebaseapp.com",
+  projectId: "tik-tok-clone-12d14",
+  storageBucket: "tik-tok-clone-12d14.appspot.com",
+  messagingSenderId: "1026811117377",
+  appId: "1:1026811117377:web:15edc11ad774f8f2fb6bc0"
+};
+
+const app = initializeApp(firebaseConfig);
